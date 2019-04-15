3ce1fb9bb4628aeae1233eebfa0b153be0e74416
Marc Davies and Marina Poltorak <marc@>
2019-04-15 13:38:15 -0700

    add code that clears the inputted number field

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
 //Business Logic --------------------------------------------------------------
 
 var secretNumber = Math.floor(Math.random() * 100);
-// console.log("secretNumber", secretNumber);
+console.log("secretNumber", secretNumber);
 
 function compareNumbers(guessedNumber, secretNumber) {
   if(guessedNumber === secretNumber) {
     $("#output").text("Congratulations, you've WON!")
-    //console.log("compareNumbers function", "Congratulations, you've WON!")
+    console.log("compareNumbers function", "Congratulations, you've WON!")
   } else if (guessedNumber > secretNumber){
     $("#output").text("Guess again, it's lower!");
-    //console.log("Guess again, it's lower!")
+    console.log("Guess again, it's lower!")
   } else if(guessedNumber < secretNumber) {
     $("#output").text("Guess again, it's higher!");
-    //console.log("Guess again, it's higher");
+    console.log("Guess again, it's higher");
   }
 };
 
 //User Interface Logic ---------------------------------------------------------
 
 $(document).ready(function() {
-  $("form#userInput").submit(function(event) {
+  $("#userInput").submit(function(event) {
     event.preventDefault();
-    var guessedNumber = parseInt($("input#guessedNumber").val());
+    var guessedNumber = parseInt($("#guessedNumber").val());
     console.log("guessedNumber", guessedNumber);
+    // var rangeNumber = parseInt($("input#rangeNumber").val());
+    // console.log("rangeNumber", rangeNumber);
     compareNumbers(guessedNumber, secretNumber);
     $("#output").show();
+    $("#guessedNumber").val("");
  });
 });
